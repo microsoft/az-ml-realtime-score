618fd8cb34849cfdca96ed78b2a830af90902327
Dan <dciborow@microsoft.com>
2020-02-13 14:57:30 -0500

    Adding Pandas

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -4,6 +4,8 @@ ai-architecture-template - test_notebooks.py
 Copyright (c) Microsoft Corporation. All rights reserved.
 Licensed under the MIT License.
 """
+import re
+
 import papermill as pm
 from azure_utils.machine_learning.utils import load_configuration
 from junit_xml import TestSuite, TestCase
@@ -33,12 +35,8 @@ def test_00_aml_configuration():
     with open('notebooks/00_AMLConfiguration.output_ipynb', 'r') as file:
         data = file.read()
 
-        import re
-        searchObj = re.findall(regex, data)
-
         test_cases = []
-
-        for group in searchObj:
+        for group in re.findall(regex, data):
             test_cases.append(
                 TestCase(name=group[0] + " creation", classname='00_AMLConfiguration', elapsed_sec=float(group[2]),
                          status="Success"))
