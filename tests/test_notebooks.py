9313b2579ecc76dc41c89c9adbabcddc938d2d3c
Dan <dciborow@microsoft.com>
2020-02-14 01:53:44 -0500

    Adding Tests

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -4,7 +4,6 @@ ai-architecture-template - test_notebooks.py
 Copyright (c) Microsoft Corporation. All rights reserved.
 Licensed under the MIT License.
 """
-import os
 import re
 
 import papermill as pm
@@ -45,32 +44,35 @@ def test_00_aml_configuration(record_nunit_property, add_nunit_attachment):
             TestSuite.to_file(f, [ts], prettyprint=False)
 
 
-def test_01_aml_configuration():
-    run_notebook('01_DataPrep.ipynb', '01_DataPrep.output_ipynb')
+def test_01_aml_configuration(add_nunit_attachment):
+    run_notebook('01_DataPrep.ipynb', '01_DataPrep.output_ipynb', add_nunit_attachment=add_nunit_attachment)
 
 
-def test_02_aml_configuration():
-    run_notebook('02_TrainOnLocal.ipynb', '02_TrainOnLocal.output_ipynb')
+def test_02_aml_configuration(add_nunit_attachment):
+    run_notebook('02_TrainOnLocal.ipynb', '02_TrainOnLocal.output_ipynb', add_nunit_attachment=add_nunit_attachment)
 
 
-def test_03_aml_configuration():
-    run_notebook('03_DevelopScoringScript.ipynb', '03_DevelopScoringScript.output_ipynb')
+def test_03_aml_configuration(add_nunit_attachment):
+    run_notebook('03_DevelopScoringScript.ipynb', '03_DevelopScoringScript.output_ipynb',
+                 add_nunit_attachment=add_nunit_attachment)
 
 
-def test_04_aml_configuration():
-    run_notebook('04_CreateImage.ipynb', '04_CreateImage.output_ipynb')
+def test_04_aml_configuration(add_nunit_attachment):
+    run_notebook('04_CreateImage.ipynb', '04_CreateImage.output_ipynb', add_nunit_attachment=add_nunit_attachment)
 
 
-def test_05_aml_configuration():
-    run_notebook('05_DeployOnAKS.ipynb', '05_DeployOnAKS.output_ipynb')
+def test_05_aml_configuration(add_nunit_attachment):
+    run_notebook('05_DeployOnAKS.ipynb', '05_DeployOnAKS.output_ipynb', add_nunit_attachment=add_nunit_attachment)
 
 
-def test_06_aml_configuration():
-    run_notebook('06_SpeedTestWebApp.ipynb', '06_SpeedTestWebApp.output_ipynb')
+def test_06_aml_configuration(add_nunit_attachment):
+    run_notebook('06_SpeedTestWebApp.ipynb', '06_SpeedTestWebApp.output_ipynb',
+                 add_nunit_attachment=add_nunit_attachment)
 
 
-def test_07_aml_configuration():
-    run_notebook('07_RealTimeScoring.ipynb', '07_RealTimeScoring.output_ipynb')
+def test_07_aml_configuration(add_nunit_attachment):
+    run_notebook('07_RealTimeScoring.ipynb', '07_RealTimeScoring.output_ipynb',
+                 add_nunit_attachment=add_nunit_attachment)
 
 
 def run_notebook(input_notebook, output_notebook, parameters=None, add_nunit_attachment=None):
@@ -85,19 +87,17 @@ def run_notebook(input_notebook, output_notebook, parameters=None, add_nunit_att
     parameters : Optional Parameters to pass to papermill
     """
 
-    results = pm.execute_notebook(
-        "notebooks/" + input_notebook,
-        "notebooks/" + output_notebook,
-        parameters=parameters,
-        kernel_name="az-ml-realtime-score"
-    )
+    try:
+        results = pm.execute_notebook(
+            "notebooks/" + input_notebook,
+            "notebooks/" + output_notebook,
+            parameters=parameters,
+            kernel_name="az-ml-realtime-score"
+        )
 
-    print("\n")
-    print("[[ATTACHMENT|" + os.path.abspath("notebooks/" + output_notebook) + "]]")
-    print("\n")
-    if add_nunit_attachment is not None:
-        add_nunit_attachment('notebooks/' + output_notebook, output_notebook)
-
-    for cell in results.cells:
-        if cell.cell_type is "code":
-            assert not cell.metadata.papermill.exception, "Error in Python Notebook"
+        for cell in results.cells:
+            if cell.cell_type is "code":
+                assert not cell.metadata.papermill.exception, "Error in Python Notebook"
+    finally:
+        if add_nunit_attachment is not None:
+            add_nunit_attachment('notebooks/' + output_notebook, output_notebook)
